9cdf5ec2601f332bce2cfc1b5128d3e60b714883
tichima <tchima@usc.edu>
2018-03-15 04:56:45 -0700

    find and add patient updated

--- oracleprac.py
+++ oracleprac.py
@@ -22,67 +22,79 @@ def connectDB(): # establish connection to Oracle cloud DB
 	
 	
 	print 'Welcome to TChima Clinic.'
-	menu = '''Please select an option:
-|| 1 - add patient || 2 - find patient || 3 - list all patients 
-|| 4 - export patient information to XML 
-|| 0 - exit
-'''
+	menu = '''
+Please select an option:
+|| 1 - add patient   || 2 - find patient   || 3 - list all patients 
+|| 4 - export patient information to XML   || 0 - exit
+Option: '''
 	
 	option = 1
 	
-	while(option):
-		option = input(menu)
-	
-		if option is 1:
-			addPatient(cur)
-			
-		if option is 2:
-			findPatient(cur)
-			
-		if option is 3:
-			listAllPatient(cur)
-			
-		if option is 4:
-			exportPatient(cur)
-	
-	print "Exiting TChima Clinic...Goodbye!"
-	
-	con.close()
+	try:
+		while(option):
+			option = input(menu)
+		
+			if option is 1:
+				addPatient(cur)
+				
+			if option is 2:
+				findPatient(cur)
+				
+			if option is 3:
+				listAllPatient(cur)
+				
+			if option is 4:
+				exportPatient(cur)
+				
+	finally:
+		print "\nExiting TChima Clinic...Goodbye!"
+		con.commit()
+		con.close()
 
 def addPatient(cur):
-	# statement = 'select * from patient order by id'
-	# cur.execute(statement)
-	# id = 0
-	# for result in cur:
-		# id = id+1
-		
-	# id = id+1
-	# name = raw_input("Enter Name: First Last")
+	name = raw_input("Enter Name: First Last\n")
+	name = name.strip().title()
 	
-	# while (len(name.split()) < 2):
-		# print "Must enter first and last name"
-		# name = raw_input("Enter Name: First Last")
+	while (len(name.split()) < 2):
+		print "Must enter first and last name"
+		name = raw_input("Enter Name: First Last\n")
+		name = name.strip().title()
 	
-	# statement = 'insert into patient (id, fname, lname) values ('+str(id)+', '+name.split()[0]+', '+name.split()[1]+')'
-	# print statement
-	# cur.execute(statement)
-	pass
+	statement = "insert into patient (fname, lname) values ('"+name.split()[0]+"', '"+name.split()[1]+"')"
+	cur.execute(statement)
+	print "Patient added"
 
 def findPatient(cur):
-	pass
-
+	name = raw_input("Enter Name: First or Last\n")
+	name = name.strip().title()
+	
+	if (len(name.split()) < 2):
+		statement = "select * from patient where fname='"+name+"' or lname='"+name+"'"
+	
+	else:
+		statement = "select * from patient where fname='"+name.split()[0]+"' and lname='"+name.split()[1]+"'"
+	
+	#print statement
+	cur.execute(statement)
+	
+	count = 0
+	for result in cur:
+		print result
+		count = count+1
+	
+	if count is 0:
+		print "No patients found"
+	
 def listAllPatient(cur):
 	cur.execute('select * from patient order by id')
 	for result in cur:
 		print result
-	
-	
+
 def exportPatient(cur):
 	pass
 
 def debug(cur):
 	print con.version
-
 
 if __name__ == "__main__": # calls the function main()
     main()
